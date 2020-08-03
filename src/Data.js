baf2f76c416cbc3e1798432e6cc5312b7fec2228
Sunjam99 <simzmusaz@gmail.com>
2020-08-03 06:48:25 +0200

    finalcomit

---
+++ src/Data.js
@@ -0,0 +1,56 @@
+export default {
+  users: [
+    { username: "regular", accessLevel: 1, password: "password" },
+    { username: "admin", accessLevel: 0, password: "password" }
+  ],
+  initProducts: [
+    {
+      name: "Cape Flight",
+      stock: 10,
+      price: 39.99,
+      shortDesc: "A different view to the Cape",
+      description:
+        "Taken around just before the sunset as the glowing red hit the Cape Flat walls to enleash a beautiful red onto the already red neighbourhood"
+    },
+    {
+      name: "Seaing Point",
+      stock: 20,
+      price: 25.99,
+      shortDesc: "Sideview of the beautiful Sea Point",
+      description:
+        "Is wasn't a matter of orientation but a point of seeing a different perseptive on Sea Point more of the road less travelled."
+    },
+    {
+      name: "Hout Bay",
+      stock: 15,
+      price: 15.99,
+      shortDesc: "Nulla facilisi. Curabitur at lacus ac velit ornare lobortis.",
+      description:
+        "Cras sagittis. Praesent nec nisl a purus blandit viverra. Ut leo. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Fusce a quam."
+    },
+    {
+      name: "TIpping point",
+      stock: 5,
+      price: 10.99,
+      shortDesc: "Nulla facilisi. Curabitur at lacus ac velit ornare lobortis.",
+      description:
+        "Cras sagittis. Praesent nec nisl a purus blandit viverra. Ut leo. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Fusce a quam."
+    },
+    {
+      name: "Station",
+      stock: 5,
+      price: 45.99,
+      shortDesc: "Train travellers",
+      description:
+        "The most used form of transport of travel in the cape yet the most underrated"
+    },
+    {
+      name: "Other Side",
+      stock: 8,
+      price: 79.99,
+      shortDesc: "The Ghetto in the Sea",
+      description:
+        "The one of many of the ghettos in the Cape the overlooked."
+    }
+  ]
+};
